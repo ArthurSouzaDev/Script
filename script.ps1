b61301f2baf36661136da9edc178f35c07303b18
Arthur Almeida <almeidaarthur128@gmail.com>
2025-10-23 00:17:47 -0300

    Atualizando e colocando para permitir automaticamente.

--- script.ps1
+++ script.ps1
@@ -1,17 +1,18 @@
-# Conteúdo do arquivo script.ps1
+# Conteúdo do arquivo script.ps1 (CORRIGIDO)
 
 Write-Host "Iniciando instalação de aplicativos..."
 
 # Essenciais
-winget install --id=Google.Chrome -e --silent
-winget install --id=AnyDesk.AnyDesk -e --silent
+winget install --id=Google.Chrome -e --silent --accept-source-agreements --accept-package-agreements
+winget install --id=AnyDesk.AnyDesk -e --silent --accept-source-agreements --accept-package-agreements
+winget install --id=7zip.7zip -e --silent --accept-source-agreements --accept-package-agreements
 
 # Office (Microsoft 365)
-winget install --id=Microsoft.Office -e --silent
+winget install --id=Microsoft.Office -e --silent --accept-source-agreements --accept-package-agreements
 
 # Ferramentas Power User
-winget install --id=Microsoft.PowerToys -e --silent
-winget install --id=Microsoft.WindowsTerminal -e --silent
+winget install --id=Microsoft.PowerToys -e --silent --accept-source-agreements --accept-package-agreements
+winget install --id=Microsoft.WindowsTerminal -e --silent --accept-source-agreements --accept-package-agreements
 
 Write-Host "Instalação de aplicativos concluída!"
 
